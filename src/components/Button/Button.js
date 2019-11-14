1917a2cfd1f1b008e40188cb3f45a66f031fee2f
Hello-Kozloff <hello.kozloff@gmail.com>
2019-11-14 15:22:36 +0300

    Create Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import classes from './Button.module.css';
+
+const Button = (props) => {
+  const {
+    type,
+    theme,
+    size,
+    wide,
+    disabled,
+    children,
+    onClick,
+  } = props;
+
+  const componentClass = classNames(
+    classes.component,
+    theme ? classes[theme] : null,
+    size ? classes[size] : null,
+    wide ? classes.wide : null,
+    disabled ? classes.disabled : null,
+  );
+
+  return (
+    <button
+      type={type}
+      disabled={disabled}
+      className={componentClass}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  type: PropTypes.oneOf([
+    'button',
+    'submit',
+    'reset',
+  ]),
+  theme: PropTypes.oneOf([
+    'primary',
+    'secondary',
+    'tertiary',
+  ]),
+  size: PropTypes.oneOf([
+    'small',
+    'medium',
+    'large',
+  ]),
+  wide: PropTypes.bool,
+  disabled: PropTypes.bool,
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array,
+  ]).isRequired,
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  type: 'button',
+  theme: 'primary',
+  size: 'medium',
+  wide: false,
+  disabled: false,
+  onClick: () => false,
+};
+
+export default Button;
