5d5b19f4f8c707b8fffe72961e4f5b1930caa7ef
Hello-Kozloff <hello.kozloff@gmail.com>
2019-11-14 20:48:07 +0300

    Fix index file in forms

--- src/components/forms/index.js
+++ src/components/forms/index.js
@@ -1,3 +1,3 @@
-export { default as AuthorizationForm } from './authorization/Authorization';
-export { default as RegisterForm } from './register/Register';
-export { default as RecoveryForm } from './recovery/Recovery';
+export { default as AuthorizationForm } from './authorization';
+export { default as RegisterForm } from './register';
+export { default as RecoveryForm } from './recovery';
