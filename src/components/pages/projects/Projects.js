6cc8899a2607a767fb530af0eb578c357d40bf82
Hello-Kozloff <hello.kozloff@gmail.com>
2019-11-15 16:06:09 +0300

    Add projects page layout

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -1,17 +1,52 @@
 import React from 'react';
-import classes from '../../../styles/Page.module.css';
+
+import { Button } from '../../common';
+
+import pageClasses from '../../../styles/Page.module.css';
+import projectsClasses from './Projects.module.css';
 
 class ProjectsPage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      projects: [],
+    };
   }
 
   render() {
+    const { projects } = this.state;
+
+    const projectsTotal = projects.length;
+
     return (
-      <div className={classes.container}>
-        hello world
+      <div className={pageClasses.container}>
+        <div className={projectsClasses.component}>
+          <header className={projectsClasses.header}>
+            <h1 className={projectsClasses.heading}>
+              Все проекты(
+              {projectsTotal}
+              ):
+            </h1>
+            <div className={projectsClasses.actions}>
+              <Button
+                type="button"
+                theme="primary"
+                size="small"
+              >
+                Создать проект
+              </Button>
+            </div>
+          </header>
+
+          {
+            projectsTotal === 0 ? (
+              <div className={projectsClasses.empty}>
+                Вы еще не создали ни одного проекта. Самое время это сделать!
+              </div>
+            ) : null
+          }
+        </div>
       </div>
     );
   }
