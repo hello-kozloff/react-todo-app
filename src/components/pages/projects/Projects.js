a97eaedcfae7feb8e0605013523f9b0d16155682
Hello-Kozloff <hello.kozloff@gmail.com>
2019-11-15 17:05:53 +0300

    Changed layout in projects page

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button } from '../../common';
+import { Button, ProjectCard } from '../../common';
 
 import pageClasses from '../../../styles/Page.module.css';
 import projectsClasses from './Projects.module.css';
@@ -10,7 +10,28 @@ class ProjectsPage extends React.Component {
     super(props);
 
     this.state = {
-      projects: [],
+      projects: [
+        {
+          slug: 'first-project',
+          name: 'Мой первый проект',
+          thumbnailUrl: 'https://images.unsplash.com/photo-1573805797307-d6975847411b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=100',
+        },
+        {
+          slug: 'development-of-bridge',
+          name: 'Очнь длинный заголовок проекта, который обязательно должен быть обрезан, сейчас мы это сделаем',
+          thumbnailUrl: 'https://images.unsplash.com/photo-1568475734433-6d2c050b5749?ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80',
+        },
+        {
+          slug: 'tour-in-usa',
+          name: 'Тур по Америке',
+          thumbnailUrl: 'https://images.unsplash.com/photo-1568822602205-62ac63d1268f?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+        },
+        {
+          slug: 'travel-dubai',
+          name: 'Путешествие в Дубаи',
+          thumbnailUrl: 'https://images.unsplash.com/photo-1568822577231-6e3456d0363e?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
+        },
+      ],
     };
   }
 
@@ -38,13 +59,26 @@ class ProjectsPage extends React.Component {
               </Button>
             </div>
           </header>
-
           {
-            projectsTotal === 0 ? (
+            projectsTotal !== 0 ? (
+              <div className={projectsClasses.projects}>
+                {
+                  projects.map((project) => (
+                    <div key={project.slug} className={projectsClasses.project}>
+                      <ProjectCard
+                        slug={project.slug}
+                        name={project.name}
+                        thumbnailUrl={project.thumbnailUrl}
+                      />
+                    </div>
+                  ))
+                }
+              </div>
+            ) : (
               <div className={projectsClasses.empty}>
                 Вы еще не создали ни одного проекта. Самое время это сделать!
               </div>
-            ) : null
+            )
           }
         </div>
       </div>
