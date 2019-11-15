67c26c00b8ce42e4ff1b19dec2d810bcf5eda5d9
Hello-Kozloff <hello.kozloff@gmail.com>
2019-11-16 02:58:03 +0300

    Add TasksContainer in index.js

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,4 +1,6 @@
 export { default as Button } from './Button';
 export { default as Input } from './Input';
+export { default as TasksContainer } from './TasksContainer';
+
 // eslint-disable-next-line import/no-cycle
 export { default as ProjectCard } from './ProjectCard';
